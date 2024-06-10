dc0acc6e680b341876cb4147f14e57df212fd025
flowerburied <2842511561@qq.com>
2024-06-10 14:57:21 +0800

    6 JSX List.map

--- react18-project/react-basic/src/App.js
+++ react18-project/react-basic/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+const count = 100;
 
-function App() {
+const getName = () => {
+  return "MyName";
+};
+const list = [
+  { id: 1, name: "react" },
+  { id: 2, name: "vue" },
+];
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      this is app{count} {getName()} {new Date().getDate()}
+      <div style={{ color: "red" }}>this is red</div>
+      <div>
+        {list.map((item) => (
+          <div key={item.id}>{item.name}</div>
+        ))}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
