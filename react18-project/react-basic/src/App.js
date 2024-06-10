fbd4a2e82d3e2ef8d6b7084ed3acf20430a3d975
flowerburied <2842511561@qq.com>
2024-06-10 17:31:11 +0800

    className dynamic

--- react18-project/react-basic/src/App.js
+++ react18-project/react-basic/src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+import "./index.css";
 const count = 100;
 
 const getName = () => {
@@ -8,16 +10,58 @@ const list = [
   { id: 2, name: "vue" },
 ];
 
+const isLogin = true;
+
+const articleType = 1;
+const getArticleTem = () => {
+  if (articleType === 0) {
+    return <div>my is no image article</div>;
+  } else if (articleType === 1) {
+    return <div>my is only one image type</div>;
+  }
+};
+
+const Button = () => {
+  return <button>click me!</button>;
+};
+
 const App = () => {
+  const handleClick = (val, e) => {
+    // console.log("button clicked", val, e);
+    setCounts(counts + 1);
+    setForm({ ...form, name: "vue" });
+  };
+  const [form, setForm] = useState({ name: "jack" });
+  const [counts, setCounts] = useState(0);
+  const style = {
+    color: "red",
+    fontSize: "50px",
+  };
+  const setClassName = () => {
+    return "active";
+  };
+
   return (
     <div className="App">
       this is app{count} {getName()} {new Date().getDate()}
       <div style={{ color: "red" }}>this is red</div>
-      <div>
+      <div style={style}>
         {list.map((item) => (
           <div key={item.id}>{item.name}</div>
         ))}
       </div>
+      <div className="foo">
+        {/* 逻辑与&& */}
+        {isLogin && <span>this is span</span>}
+        {isLogin ? <span>jack</span> : <span>loading...</span>}
+      </div>
+      <div className={setClassName()}>{getArticleTem()}</div>
+      <button onClick={($event) => handleClick("jack", $event)}>
+        this is button{counts}
+        {form.name}
+      </button>
+      {Button()}
+      <Button></Button>
     </div>
   );
 };
